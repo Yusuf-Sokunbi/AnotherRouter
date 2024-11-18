5be534505d56d93252455e490bac01429c4e207d
Yusuf-Sokunbi <adeloaoo87@gmail.com>
2024-11-18 08:27:01 +0100

    adding pages to root of page and Outlet

--- src/App.jsx
+++ src/App.jsx
@@ -25,12 +25,17 @@ import EventsPage from "./pages/Events";
 import EventDetailPage from "./pages/EventDetailPage";
 import NewEventPage from "./pages/NewEventPage";
 import EditEventPage from "./pages/EditEventPage";
-const router = createBrowserRouter([
-{path:'/', element:<HomePage />},
-{path:'/events', element:<EventsPage />},
-{path:'/events/:eventId', element:<EventDetailPage />},
-{path:'/events/new', element:<NewEventPage />},
-{path:'/events/:eventId/:edit', element:<EditEventPage />}
+import RootPage from "./pages/RootPage";
+const router = createBrowserRouter([{
+  path:'', element:<RootPage />, 
+  children:[
+    {path:'/', element:<HomePage />},
+    {path:'/events', element:<EventsPage />},
+    {path:'/events/:eventId', element:<EventDetailPage />},
+    {path:'/events/new', element:<NewEventPage />},
+    {path:'/events/:eventId/:edit', element:<EditEventPage />}
+  ]
+},
 ])
   
 function App() {
